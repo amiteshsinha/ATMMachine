40fda120b95affb4ed30a8f627112d0c432fcae4
amiteshsinha <amiteshsinha88@gmail.com>
2020-09-29 00:52:58 +0530

    first commit

---
+++ src/main/java/com/personal/assignment/atmmachine/AtmmachineApplication.java
@@ -0,0 +1,13 @@
+package com.personal.assignment.atmmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmmachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmmachineApplication.class, args);
+	}
+
+}
